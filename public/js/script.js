f46cc8161fd8721bc32137e2bbe03c3202042e5a
vonlugersbutter <ayeshaumair10@gmail.com>
2021-08-28 23:55:42 -0400

    finished first question animation

---
+++ public/js/script.js
@@ -0,0 +1,203 @@
+anime({
+    targets: '#inflammatory',
+    width: 20,
+    delay: 1000,
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#bowel',
+    width: 40,
+    delay: 1500,
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#disease',
+    width: 40,
+    delay: 2000,
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#adherent',
+    width: 40,
+    delay: 6000,
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#invasive',
+    width: 20,
+    delay: 6500,
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#escherichia',
+    width: 38,
+    delay: 7000,
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#coli',
+    width: 40,
+    delay: 7500,
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#inflammatory',
+    translateY: [
+        {value: -600}
+    ],
+    top: [
+        {value: 650, delay: 3000},
+        {value: 900, delay: 7500}
+    ],
+    left: [
+        {value: 1000, delay: 3000},
+        {value: 370, delay: 7500}
+    ],
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#bowel',
+    translateY: [
+        {value: -600}
+    ],
+    top: [
+        {value: 650, delay: 3000},
+        {value: 900, delay: 7500}
+    ],
+    left: [
+        {value: 1030, delay: 3000},
+        {value: 400, delay: 7500}
+    ],
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#disease',translateY: [
+        {value: -600}
+    ],
+    top: [
+        {value: 650, delay: 3000},
+        {value: 900, delay: 7500}
+    ],
+    left: [
+        {value: 1080, delay: 3000},
+        {value: 450, delay: 7500}
+    ],
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#adherent',
+    translateY: [
+        {value: -600, delay: 4000}
+    ],
+    top: [
+        {value: 750, delay: 8500},
+        {value: 800, delay: 1500}
+    ],
+    left: [
+        {value: 990, delay: 8500},
+        {value: 600, delay: 1500}
+    ],
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#invasive',
+    translateY: [
+        {value: -600, delay: 4000}
+    ],
+    top: [
+        {value: 750, delay: 8500},
+        {value: 800, delay: 1500}
+    ],
+    left: [
+        {value: 1040, delay: 8500},
+        {value: 650, delay: 1500}
+    ],
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#escherichia',
+    translateY: [
+        {value: -600, delay: 4000}
+    ],
+    top: [
+        {value: 750, delay: 8500},
+        {value: 800, delay: 1500}
+    ],
+    left: [
+        {value: 1065, delay: 8500},
+        {value: 675, delay: 1500}
+    ],
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#coli',
+    translateY: [
+        {value: -600, delay: 4000}
+    ],
+    top: [
+        {value: 750, delay: 8500},
+        {value: 800, delay: 1500}
+    ],
+    left: [
+        {value: 1110, delay: 8500},
+        {value: 720, delay: 1500}
+    ],
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#role1',
+    translateY: [
+        {value: -500, delay: 8500}
+    ],
+    opacity: [
+        {value: 1, delay: 9500}
+    ],
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#role2',
+    translateY: [
+        {value: -400, delay: 8500}
+    ],
+    opacity: [
+        {value: 1, delay: 10500}
+    ],
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#role3',
+    translateY: [
+        {value: -400, delay: 8500}
+    ],
+    translateX: [
+        {value: 400, delay: 10500}
+    ],
+    opacity: [
+        {value: 1, delay: 10500}
+    ],
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '.rectangle',
+    translateX: [
+        {value: -2500, duration: 5000, delay: 11000}
+    ],
+    easing: 'easeInOutExpo'
+});
